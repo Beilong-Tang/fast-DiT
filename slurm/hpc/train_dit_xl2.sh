942f49799e83745c185696e416fcabe19666276f
Beilong Tang <btang5@ncsu.edu>
2026-09-09 15:27:07 -0400

    update code for training

--- slurm/hpc/train_dit_xl2.sh
+++ slurm/hpc/train_dit_xl2.sh
@@ -28,6 +28,6 @@ nvidia-smi --query-gpu=timestamp,utilization.gpu,utilization.memory,memory.used,
 vae='ema'
 save_dir="/share/m1/btang5/workspace/fast_dit/vae_${vae}_latents_hf"
 epochs=80
-accelerate launch --multi_gpu --num_processes 2 --mixed_precision fp16 train.py --model DiT-XL/2 \
+accelerate launch --multi_gpu --num_processes 4 --mixed_precision fp16 train.py --model DiT-XL/2 \
     --feature-path "$save_dir" --resume \
-    --epochs "$epochs"
+    --epochs "$epochs" 
